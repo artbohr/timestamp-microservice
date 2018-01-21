126c75bb66ba09683a68aafef3b5e2ef51b2ca7c
artbohr <bohrcs@gmail.com>
2018-01-21 05:26:39 +0100

    Add ES6 syntax and formatting

--- server.js
+++ server.js
@@ -1,27 +1,18 @@
-// server.js
-// where your node app starts
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-// init project
-var express = require("express");
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require("cors");
 app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 // Timestamp API endpoint...
-app.get("/api/timestamp/:date_string?", function(req, res) {
+app.get("/api/timestamp/:date_string?", (req, res) => {
   // if it's undefined set current time, else pass the provided value
-  var date = req.params.date_string === undefined ? new Date().toString() : req.params.date_string;
+  const date = req.params.date_string === undefined ? new Date().toString() : req.params.date_string;
 
   // check that it's not a number
   if (isNaN(date)) {
@@ -34,6 +25,6 @@ app.get("/api/timestamp/:date_string?", function(req, res) {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
+const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
